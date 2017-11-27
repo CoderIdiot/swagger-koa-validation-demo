ce6f307744da8edd8e72d307b49fbf579e44b606
HaiChiang <rq8686@qq.com>
2017-11-27 13:10:52 +0800

    Done For Simple API test.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,34 @@
+import "babel-polyfill";
 import Koa from 'koa'
+import ValidatorFactory from 'swagger-koa-validate'
+
+/*
+ * Read the API, and init the validate
+ */
+import * as fs from "fs-extra"
+const YAML = require('yamljs')
+const $RefParser = require('json-schema-ref-parser')
+var parser = new $RefParser()
+
+const specStr = fs.readFileSync(__dirname + '/../api/api.yaml', 'utf-8')
+const spec = YAML.parse(specStr)
+var validate = ValidatorFactory(spec)
+
 
 const app = new Koa()
 
-
-app.use(ctx => ctx.body = 'hello world')
+app.use(async (ctx, next) => {
+    try {
+        await next()
+    } catch (error) {
+        ctx.status = error.status || 400
+        ctx.body = { message: error.message }
+        console.log('Error', error.status, error.message)
+    }
+})
+app.use(validate)
+app.use(ctx => {
+    ctx.body = 'hello world'
+})
 
 app.listen(3000)
